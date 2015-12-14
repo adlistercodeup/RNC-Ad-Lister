cc98fee439f37dc049dca1a34da7a1d5ebf6996c
Reni <reni.fagbamiye@gmail.com>
2015-12-14 15:21:41 -0600

    created the files

---
+++ views/partials/header.php
@@ -0,0 +1,3 @@
+<header>
+	<h1>Adlist</h1>
+</header>
---
+++ views/partials/navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Banana-Split</a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#jobs">clothes</a></li>
+        <li><a href="#toys">toys</a></li>
+        <li><a href="#furniture">furniture</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
